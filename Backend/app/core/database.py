70cbfa0b9c235a4a9282cf07507aac6db07687fa
oreltz1 <oreltz1@ac.sce.ac.il>
2026-04-17 15:01:48 +0300

    Fix CI: add requirements.txt and fix database default env vars

--- Backend/app/core/database.py
+++ Backend/app/core/database.py
@@ -5,8 +5,8 @@ import certifi
 import ssl
 load_dotenv()
 
-MONGODB_URL = os.getenv("MONGODB_URL")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
+MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "test_db")
 
 client = AsyncIOMotorClient(
     MONGODB_URL,
